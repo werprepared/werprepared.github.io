ccd558fccfe9371608e7cbf579b34ac4fd829222
zachpanqu <werpreparedapp@gmail.com>
2024-11-12 11:23:41 -0600

    change

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2774587323"
+    serviceWorkerVersion: "2458780488"
   }
 });
